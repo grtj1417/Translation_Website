3ad926427e4aa81add45e760c78124061f486925
JoshuaLee <joshualee032190@gmail.com>
2024-07-13 23:08:16 +0800

    modify server addr

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		host: "140.116.245.147",
+		port: "5173"
+		
+
+	},
   plugins: [react()],
 })
